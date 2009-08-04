8888408c1249151748cf8bb6e3ea2ea064ce3b5b
christian.blunden <christian.blunden@localhost>
2009-08-04 21:02:09 +0100

    Initial Check in all files

---
+++ src/Yadic.Tests/SimpleContainerTests.cs
@@ -0,0 +1,271 @@
+﻿using Yadic;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace Container.Tests
+{
+    [TestFixture]
+    public class ContainerTests
+    {
+        [Test]
+        public void ShouldChainContainersThroughMissingAction()
+        {
+            IContainer child = new SimpleContainer();
+            child.Add<IThing, ThingWithNoDependencies>();
+
+            IContainer parent = new SimpleContainer(child.Resolve);
+
+            IThing thing = parent.Resolve<IThing>();
+
+            Assert.That(thing, Is.InstanceOfType(typeof(ThingWithNoDependencies)));
+        }
+
+        [Test]
+        public void ShouldResolveByType()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<IThing, ThingWithNoDependencies>();
+
+            object thing = container.Resolve(typeof (IThing));
+
+            Assert.That(thing, Is.InstanceOfType(typeof(ThingWithNoDependencies)));
+        }
+
+        [Test]
+        public void ShouldCallMissingMethodWhenItemNotFound()
+        {
+            bool wasCalled = false;
+            IContainer container = new SimpleContainer(t =>
+                                                           {
+                                                               wasCalled = true;
+                                                               return null;
+                                                           });
+            container.Resolve<IThing>();
+
+            Assert.That(wasCalled, Is.True);
+        }
+
+        [Test]
+        public void ShouldOnlyCallCreationLambdaOnce()
+        {
+            int count = 0;
+            IContainer container = new SimpleContainer();
+            
+            container.Add<IThing>(() => {
+                  count++;
+                  return new ThingWithNoDependencies();
+              });
+
+            container.Resolve<IThing>();
+            IThing thing = container.Resolve<IThing>();
+
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ShouldDecorateAnExistingComponent()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<IThing, ThingWithNoDependencies>();
+            container.Decorate<IThing, DecoratedThing>();
+
+            var thing = container.Resolve<IThing>();
+
+            Assert.That(thing, Is.InstanceOfType(typeof(DecoratedThing)));
+            Assert.That(thing.Dependency, Is.InstanceOfType(typeof(ThingWithNoDependencies)));
+        }
+        [Test]
+        public void ShouldAddAndReolveByConcrete()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<IThing>(() => new ThingWithNoDependencies());
+
+            var thing = container.Resolve<IThing>();
+
+            Assert.That(thing, Is.InstanceOfType(typeof(ThingWithNoDependencies)));
+        }
+
+        [Test]
+        public void ShouldAddAndResolveByInterface()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<IThing, ThingWithNoDependencies>();
+
+            var thing = container.Resolve<IThing>();
+
+            Assert.That(thing, Is.InstanceOfType(typeof(ThingWithNoDependencies)));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ContainerException))]
+        public void ShouldThrowExceptionIfAddSameTypeTwice()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<MyThing>();
+            container.Add<MyThing>();
+            Assert.Fail("Should have thrown exception");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ContainerException))]
+        public void ResolveShouldThrowExceptionIfTypeNotInContainer()
+        {
+            IContainer container = new SimpleContainer();
+            container.Resolve<MyThing>();
+            Assert.Fail("Should have thrown exception");
+        }
+
+        [Test]
+        public void ShouldAddAndResolveByClass()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<ThingWithNoDependencies>();
+            
+            var result = container.Resolve<ThingWithNoDependencies>();
+            
+            Assert.That(result, Is.InstanceOfType(typeof(ThingWithNoDependencies)));
+        }
+
+        [Test]
+        public void ResolveShouldReturnSameInstanceWhenCalledTwice()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<ThingWithNoDependencies>();
+
+            var result1 = container.Resolve<ThingWithNoDependencies>();
+            var result2 = container.Resolve<ThingWithNoDependencies>();
+            
+            Assert.AreSame(result1,result2);
+        }
+
+        [Test]
+        public void ShouldResolveDependencies()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<MyDependency>();
+            container.Add<ThingWithNoDependencies>();
+
+            var myThing = container.Resolve<MyDependency>();
+
+            Assert.That(myThing.Dependency, Is.InstanceOfType(typeof(ThingWithNoDependencies)));
+        } 
+
+        [Test]
+        public void ShouldRecursivelyResolveDependencies()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<MyThing>();
+            container.Add<MyDependency>();
+            container.Add<ThingWithNoDependencies>();
+
+            var myThing = container.Resolve<MyThing>();
+
+            Assert.That(myThing.Dependency, Is.InstanceOfType(typeof(MyDependency)));
+            Assert.That(myThing.Dependency.Dependency, Is.InstanceOfType(typeof(ThingWithNoDependencies)));
+        }
+
+        [Test]
+        public void ShouldResolveWithDependenciesInAnyOrder()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<MyDependency>();
+            container.Add<MyThing>();
+            container.Add<ThingWithNoDependencies>();
+
+            var myThing = container.Resolve<MyThing>();
+
+            Assert.That(myThing.Dependency, Is.InstanceOfType(typeof(MyDependency)), "1st level Dependency was not fulfilled");
+            Assert.That(myThing.Dependency.Dependency, Is.InstanceOfType(typeof(ThingWithNoDependencies)), "2nd level Dependency was not fulfiled");
+        }
+
+        [Test]
+        public void ShouldResolveUsingConstructorWithMostDependencies()
+        {
+            IContainer container = new SimpleContainer();
+            container.Add<MyThingWithReverseConstructor>();
+            container.Add<ThingWithNoDependencies>();
+
+            var myThing = container.Resolve<MyThingWithReverseConstructor>();
+
+            Assert.That(myThing.Dependency, Is.Not.Null, "Wrong constructor was used");
+            Assert.That(myThing.Dependency, Is.InstanceOfType(typeof(ThingWithNoDependencies)));
+        }
+    }
+
+    internal class MyThingWithReverseConstructor
+    {
+        private readonly ThingWithNoDependencies _dependency;
+
+        public MyThingWithReverseConstructor()
+        {
+        }
+
+        public MyThingWithReverseConstructor(ThingWithNoDependencies dependency)
+        {
+            _dependency = dependency;
+        }
+
+        public ThingWithNoDependencies Dependency
+        {
+            get { return _dependency; }
+        }
+    }
+
+    internal class MyThing
+    {
+        private readonly MyDependency _dependency;
+
+        public MyThing(MyDependency dependency)
+        {
+            _dependency = dependency;
+        }
+
+        public MyDependency Dependency
+        {
+            get { return _dependency; }
+        }
+    }
+
+    internal class MyDependency
+    {
+        private readonly ThingWithNoDependencies _dependency;
+
+        public MyDependency(ThingWithNoDependencies dependency)
+        {
+            _dependency = dependency;
+        }
+
+        public ThingWithNoDependencies Dependency
+        {
+            get { return _dependency; }
+        }
+    }
+
+    internal class ThingWithNoDependencies : IThing
+    {
+        public IThing Dependency
+        {
+            get { return null; }
+        }
+    }
+
+    internal class DecoratedThing : IThing
+    {
+        private readonly IThing _wrapped;
+
+        public DecoratedThing(IThing wrapped)
+        {
+            _wrapped = wrapped;
+        }
+
+        public IThing Dependency
+        {
+            get { return _wrapped; }
+        }
+    }
+
+    internal interface IThing
+    {
+        IThing Dependency { get; }
+    }
+}
---
+++ src/Yadic/SimpleContainer.fs
@@ -0,0 +1,83 @@
+﻿#light
+
+namespace Yadic
+
+open System;;
+open System.Linq;;
+open System.Reflection;;
+open System.Collections.Generic;;
+
+type public ContainerException(s) =
+ inherit Exception(s)
+ 
+type IContainer = 
+ abstract Add<'T> : unit -> unit
+ [<OverloadID("addByInterface")>]
+ abstract Add<'Interface, 'Component> : unit -> unit
+ [<OverloadID("addByConcrete")>]
+ abstract Add<'T> : Func<obj> -> unit
+ abstract Resolve<'T> : unit -> 'T
+ abstract Resolve : Type -> obj
+ abstract Decorate<'Interface, 'Component> : unit -> unit
+
+type SimpleContainer(missingHandler:Func<Type,obj>) = 
+ let activators = new Dictionary<Type, unit->obj>()
+ 
+ let get t = 
+  try 
+   let instance = activators.[t]()
+   activators.[t] <- fun() -> instance
+   instance
+  with | :? KeyNotFoundException -> t |> missingHandler.Invoke
+  
+ let toInstance (t:Type, replacement) (p:ParameterInfo) = 
+  let shouldReplace = p.ParameterType.Equals(t)
+  match shouldReplace,replacement with
+  | true,Some(o) -> o
+  | _ -> p.ParameterType |> get
+  
+ let getParameters (t:Type) = 
+  let sortedConstructors = t.GetConstructors() |> Seq.sortBy (fun c->c.GetParameters().Count())
+  sortedConstructors.LastOrDefault().GetParameters()
+ 
+ let getArgs (t,replacement) = 
+  getParameters >> Seq.map ((t,replacement) |> toInstance) >> Seq.to_array
+  
+ let createInstance = Activator.CreateInstance : Type * obj array -> obj 
+ 
+ let create (i,c,existing)  = 
+  ( c, c |> getArgs (i,existing) ) |> createInstance
+ 
+ let addActivator (t,f) =
+  try activators.Add(t,f)
+  with | :? System.ArgumentException -> 
+   raise( new ContainerException(t.ToString() + " already added to container"))
+ 
+ let add (i,c) =
+  addActivator (i, fun() -> create (i,c,None))
+  
+ let decorate (i,c) =
+  let existing = get i
+  let result = i |> activators.Remove
+  addActivator (i, fun() -> create (i,c,Some(existing)) )
+ 
+ new() = SimpleContainer(fun (t) -> raise ( new ContainerException(t.ToString() + " not found in container") ))
+ 
+ interface IContainer with
+  member this.Add<'T>() = (typeof<'T>,typeof<'T>) |> add
+  
+  [<OverloadID("addByInterface")>]
+  member this.Add<'Interface,'Component>() = (typeof<'Interface>,typeof<'Component>) |> add 
+  
+  [<OverloadID("addByConcrete")>]
+  member this.Add<'T>(f) = (typeof<'T>, fun () -> f.Invoke() ) |> addActivator
+  
+  member this.Resolve<'T>() = unbox<'T>( typeof<'T> |> get )
+  member this.Resolve(t) = t |> get
+  member this.Decorate<'Interface,'Decorator>() = (typeof<'Interface>,typeof<'Decorator>) |> decorate
+  
+
+
+ 
+
+
